a65e61b58cdc9e41172a12d880994b4858683914
Ivan Anic <i.anic779@gmail.com>
2022-06-25 12:01:49 +0200

    Added response object

---
+++ app/Models/Response.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use stdClass;
+
+class Response
+{
+    public static function OK($data = null, string $msg = "request_success")
+    {
+        $response = new stdClass;
+        $response->status = "OK";
+        $response->msg = $msg;
+        $response->data = $data;
+
+        return response()->json($response);
+    }
+
+    public static function ERROR($data = null, string $msg = "request_error")
+    {
+        $response = new stdClass;
+        $response->status = "NOT OK";
+        $response->msg = $msg;
+        $response->data = $data;
+
+        return response()->json($response);
+    }
+
+    public static function NOT_FOUND(string $msg = "data_not_found", $data = null)
+    {
+        $response = new stdClass;
+        $response->status = "NOT FOUND";
+        $response->msg = $msg;
+        $response->data = $data;
+
+        return response()->json($response);
+    }
+
+    public static function MISSING_INPUT(string $msg = "missing_input_in_request", $data = null)
+    {
+        $response = new stdClass;
+        $response->status = "NO INPUT";
+        $response->msg = $msg;
+        $response->data = $data;
+
+        return response()->json($response);
+    }
+}
